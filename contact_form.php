903c4a1616811cb7e87fa04ac062469b1e75389c
Engr. Ajao WB <49806435+Bambillo1@users.noreply.github.com>
2023-11-12 10:50:27 +0100

    Add files via upload
    initial commit

---
+++ contact_form.php
@@ -0,0 +1,32 @@
+<?php
+// Connect to your database (replace placeholders with actual credentials)
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Handle form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // Insert data into the database (replace placeholders with actual column names)
+    $sql = "INSERT INTO contact_form (name, email, message) VALUES ('$name', '$email', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Form submitted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
---
+++ script.js
@@ -0,0 +1,6 @@
+// Add your JavaScript code here
+// For example, form validation and submission using JavaScript
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    // Add your form submission logic here
+});
